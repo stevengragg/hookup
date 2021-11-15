70260bfb26d6a2465784e03873e1cc725af0d0d9
sgragg5619 <sgragg5619@gmail.com>
2021-11-16 03:03:14 +0800

    Map server js

---
+++ hookup-server/server.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+
+/**
+ * app config
+ */
+
+
+
+
+
+/**
+ * middlewares
+ */
+
+
+
+
+/**
+ * db config
+ */
+
+
+
+
+
+/**
+ * api endpoints
+ */
+
+
+
+
+/**
+ * listeners
+ */
